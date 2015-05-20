ac9bc41746eb57000bc2aaaa43095f74ec0ee04d
Martin Brändli <martin.braendli@zkb.ch>
2015-05-20 21:29:19 +0200

    empty pages und model-object

---
+++ js/notepro.js
@@ -0,0 +1,16 @@
+/**
+ * Created by martin on 20.05.2015.
+ */
+
+var note = {
+    finished: false,
+    dateCreated: '',
+    dateFinishUntil: '',
+    dateFinished: '',
+    title: '', // maxlength = ?
+    text: '', // maxlength = ?
+    importance: 0 // 0 bis 5
+};
+
+
+
