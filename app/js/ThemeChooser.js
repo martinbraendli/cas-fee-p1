b767760b9b813f46fc84d90b34e7b68111a09781
Martin Brändli <martin.braendli@zkb.ch>
2015-05-30 21:29:57 +0200

    Themechooser in own file

--- app/js/ThemeChooser.js
+++ app/js/ThemeChooser.js
@@ -1,3 +1,17 @@
-/**
- * Created by FLX on 28.05.2015.
+/*
+ * Theme Chooser triggered on change of option tag
  */
+$("#ThemeChooser")
+    .change(function () {
+        // empty var string
+        var str = "";
+        $("select option:selected").each(function () {
+            // fill string with variable of VALUE of selected option
+            str += $(this).val() + " ";
+        });
+        // adress ID link-Css and change attribut href with string
+        $("#themeCss").attr('href', str);
+        console.log(str);
+    })
+    // trigger this on change of option
+    .trigger("change");
